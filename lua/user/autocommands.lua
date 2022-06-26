9dbc33902b382c1ed8d1faaeef4ab78474b9e6ac
Kareem Hisham <safariknight2@gmail.com>
2022-06-26 22:16:43 +0200

    First commit

---
+++ lua/user/autocommands.lua
@@ -0,0 +1,48 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Use 'q' to quit from common plugins
+autocmd({ "FileType" }, {
+  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
+  callback = function()
+    vim.cmd [[
+      nnoremap <silent> <buffer> q :close<CR> 
+      set nobuflisted 
+    ]]
+  end,
+})
+
+-- Remove statusline and tabline when in Alpha
+autocmd({ "User" }, {
+  pattern = { "AlphaReady" },
+  callback = function()
+    vim.cmd [[
+      set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+      set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
+    ]]
+  end,
+})
+
+-- Set wrap and spell in markdown and gitcommit
+autocmd({ "FileType" }, {
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
+
+-- Fixes Autocomment
+autocmd({ "BufWinEnter" }, {
+  callback = function()
+    vim.cmd "set formatoptions-=cro"
+  end,
+})
+
+-- Highlight Yanked Text
+autocmd({ "TextYankPost" }, {
+  callback = function()
+    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+  end,
+})
